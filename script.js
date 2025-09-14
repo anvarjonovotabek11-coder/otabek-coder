9b9c3b8cddc40284aa7c723c9c5dc60aa4ea0684
anvarjonovotabek11-coder <anvarjonovotabek11@gmail.com>
2025-09-14 11:04:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const quotes = [
+  "Hech qachon taslim bo‘lma!",
+  "Bugun qilgan mehnating ertaga natija beradi.",
+  "Sabrlik bo‘lsang, hammasiga erishasan.",
+  "Har kuni bir qadam oldinga!",
+  "Ronaldo ham kichkina orzulardan boshlagan.",
+  "Otabek, sen ham orzularingni amalga oshirishing mumkin!",
+  "Sevgi odamni kuchli qiladi, lekin chin sevgi odamni donishmand ham qiladi.",
+  "Haqiqiy sevgi masofa tan olmaydi, yurakdan yurakka yo‘l topadi.",
+  "Sevgi ishonchdan boshlanadi, sabrdan davom etadi.",
+  "Seni chin dildan sevgan odam seni hech qachon yo‘qotishni istamaydi.",
+  "Sevgi eng katta motivatsiya – kimnidir uchun yaxshiroq odam bo‘lish.",
+  "Ayriliq og‘ir, ammo u ham insonni yanada kuchliroq qiladi.",
+  "Ba’zan ayriliq – yangi boshlanish uchun eshikdir.",
+  "Ko‘z yoshlari kechagi dardni yuvadi, yangi tong esa yangi umid keltiradi.",
+  "Kim seni unutgan bo‘lsa, uni yodda tutishga arzimaydi.",
+  "Ayriliq qalbni sindiradi, lekin sindirilgan qalb ham yana sevishni o‘rganadi."
+];
